ffe44198794c1bc10e463873bdcb50c80b0b4306
Anas Sawalha <54035970+anassawalha95@users.noreply.github.com>
2021-05-02 16:30:46 +0300

    Revert "model-of-userAdress-feedback-userConfiguration"

--- src/main/java/com/WeGather/WeGather/repositories/userConfigurationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.WeGather.WeGather.repositories;
-
-import com.WeGather.WeGather.models.userConfigurationModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userConfigurationRepository extends JpaRepository<userConfigurationModel,Long> {
-}
--- src/main/java/com/WeGather/WeGather/repositories/usersIpAddressesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.WeGather.WeGather.repositories;
-
-
-import com.WeGather.WeGather.models.usersIpAddressesModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface usersIpAddressesRepository extends JpaRepository<usersIpAddressesModel, Long> {
-}

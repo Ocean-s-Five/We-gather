b00f88e3c385f526890f4187204f005c59fcdac3
aya berjawi <ayabarjawi@gmail.com>
2021-05-04 16:44:07 +0300

    add feedback and IpAdd controller and edit userConfig vontroller

--- src/main/java/com/WeGather/WeGather/controllers/Dictionary.java
+++ src/main/java/com/WeGather/WeGather/controllers/Dictionary.java
@@ -12,5 +12,4 @@ public class Dictionary {
     DictionaryRepository dictionaryRepository;
 
 
-
 }
--- src/main/java/com/WeGather/WeGather/controllers/UserConfigurationController.java
+++ src/main/java/com/WeGather/WeGather/controllers/UserConfigurationController.java
@@ -1,13 +1,17 @@
 package com.WeGather.WeGather.controllers;
 
 import com.WeGather.WeGather.models.UserConfiguration;
+import com.WeGather.WeGather.models.Users;
 import com.WeGather.WeGather.repositories.UserConfigurationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
 
 @Controller
 public class UserConfigurationController {
@@ -25,9 +29,10 @@ public class UserConfigurationController {
             @RequestParam(value = "language") String language
             , @RequestParam(value = "timeZone") String timeZone
             , @RequestParam(value = "currency") String currency
-
-    ) {
-        UserConfiguration userConfiguration = new UserConfiguration(language,timeZone,currency);
+            , Principal p
+            ) {
+        Users user = (Users) ((UsernamePasswordAuthenticationToken) p).getPrincipal();
+        UserConfiguration userConfiguration = new UserConfiguration(language,timeZone,currency,user);
         return new RedirectView("/userConfiguration");
     }
 

1a42b738393405aacf1a85ed38c21eec7676e1c6
sabbagh99 <a.sameer1999@gmail.com>
2021-05-04 11:10:24 +0300

    checkout to new branch

--- src/main/java/com/WeGather/WeGather/controllers/LocationController.java
+++ src/main/java/com/WeGather/WeGather/controllers/LocationController.java
@@ -1,11 +1,11 @@
 package com.WeGather.WeGather.controllers;
-
+import com.WeGather.WeGather.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class District {
+public class LocationController {
 
     @Autowired
-    District district;
+    LocationRepository locationRepository;
 }

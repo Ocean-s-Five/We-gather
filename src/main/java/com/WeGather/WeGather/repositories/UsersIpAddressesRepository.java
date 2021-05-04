98c62e972ee0cb8ed19b290c30d0cf44d5f939ff
anasswalha95 <anas.swalha@yahoo.com>
2021-05-04 14:48:52 +0300

    added some css

--- src/main/java/com/WeGather/WeGather/repositories/UsersIpAddressesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.WeGather.WeGather.repositories;
-
-import com.WeGather.WeGather.models.UsersIpAddresses;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersIpAddressesRepository extends JpaRepository<UsersIpAddresses, Long> {
-}
